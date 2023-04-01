ea951bfcf232c1c19e45f94fe2d859cc4a549d31
Tatakinov <tatakinov@gmail.com>
2023-04-01 21:56:25 +0900

    saori.conf,replace.conf => kagari.conf.lua。変数展開をOnTranslate時に行うようにした。SecurityLevelを考慮するようにした。

---
+++ kagari.conf.lua
@@ -0,0 +1,18 @@
+local Config  = {
+  SAORI = {
+    -- example = [[saori\example\example.dll]],
+  },
+  Replace = {
+    --[=[
+      {
+        before  = [[、]],
+        after   = [[、\w9]],
+      },
+    --]=]
+  },
+  External  = {
+    -- [[OnExample]],
+  },
+}
+
+return Config
